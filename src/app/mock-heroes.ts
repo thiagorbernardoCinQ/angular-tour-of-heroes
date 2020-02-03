ecc583efcfde03192912ef86099e7278efdd2482
t.bernardo <t.bernardo@cinq.com.br>
2020-02-03 14:22:12 -0300

    first

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Ogaith' },
+  { id: 12, name: 'Somar' },
+  { id: 13, name: 'Corona' },
+  { id: 14, name: 'Vírus' },
+  { id: 15, name: 'Vader' },
+  { id: 16, name: 'Luke' },
+  { id: 17, name: 'R2-D2' },
+  { id: 18, name: 'Iron Man' },
+  { id: 19, name: 'Raquelleira' },
+  { id: 20, name: 'CinQMan' }
+];
