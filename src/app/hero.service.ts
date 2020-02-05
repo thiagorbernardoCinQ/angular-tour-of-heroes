274554d63cbba64351d4e52714da05f1bdc7b7f4
t.bernardo <t.bernardo@cinq.com.br>
2020-02-05 14:16:06 -0300

    Routing completed

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
-import { Observable, of } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +18,10 @@ export class HeroService {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
